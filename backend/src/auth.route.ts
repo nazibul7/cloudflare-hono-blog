fc3cdc05ba964fb40c243ed9479be59741eca7a1
Hossain SK <nazibulx.hossain.sk@intel.com>
2024-10-15 15:48:27 +0530

    add schema & migartion did

--- backend/src/auth.route.ts
+++ backend/src/auth.route.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono';
+import { getPrisma } from './prisma.function';
+const auth = new Hono<{
+    Bindings:{
+        DATABASE_URL:string
+    }
+}>();
 
-const auth = new Hono();
 
-auth.get('/signup', (c) => {
-    return c.text('Hello World');
+auth.post('/signup', async (c) => {
+    try {
+        const data = await c.req.json()
+        console.log(data);
+        
+        const existingUser = await getPrisma(c.env.DATABASE_URL).user.findFirst({
+            where: {
+                email: data.email
+            }
+        })
+        if (!existingUser) {
+            return c.text("No user found")
+        }
+
+        return c.text("Hello")
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 export default auth;
